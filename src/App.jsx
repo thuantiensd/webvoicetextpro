c6fc872fe41905f434cbd5eed118cb4534eb97b0
thuantiensd <thuantiensd1999@gmail.com>
2025-06-08 20:52:30 +0900

    Added package-lock.json

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Voice Text Pro</h1>
+      <p>Welcome to Voice Text Pro application!</p>
+    </div>
+  )
+}
+
+export default App
